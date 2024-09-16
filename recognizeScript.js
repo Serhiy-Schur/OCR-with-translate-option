08e32c231122672b4908b81e918a8f7c74a8126c
S3rG0 <chur201456@gmail.com>
2024-09-16 10:15:42 +0300

    more style

--- recognizeScript.js
+++ recognizeScript.js
@@ -1,124 +1,27 @@
-// var fileInput = document.getElementById('file-input');
-// var langSelect = document.getElementById('lang-select');
-// var recognizeBtn = document.getElementById('recognize-btn');
-// var imagePreview = document.getElementById('image-preview');
-// var resultDiv = document.getElementById('result');
-// var progressAnimation = document.getElementById('progress-container')
-// var selectedFile, selectedLang;
-//
-// fileInput.addEventListener('change', function(e) {
-//     selectedFile = e.target.files[0];
-//     displayImage(selectedFile);
-// });
-//
-// function displayImage(file) {
-//     var reader = new FileReader();
-//     reader.onload = function(event) {
-//         var img = document.createElement('img');
-//         img.src = event.target.result;
-//         imagePreview.innerHTML = '';
-//         imagePreview.appendChild(img);
-//     };
-//     reader.readAsDataURL(file);
-// }
-//
-// // Обробка вставки з буфера обміну
-// window.addEventListener('paste', function(e) {
-//     var items = e.clipboardData.items;
-//     for (var i = 0; i < items.length; i++) {
-//         if (items[i].type.indexOf('image') !== -1) {
-//             var file = items[i].getAsFile();
-//             displayImage(file);
-//             selectedFile = file;
-//             break;
-//         }
-//     }
-// });
-//
-// langSelect.addEventListener('change', function(e) {
-//     selectedLang = e.target.value;
-// });
-//
-// recognizeBtn.addEventListener('click', function () {
-//     if (!selectedFile) {
-//         alert('Будь ласка, виберіть зображення');
-//         return;
-//     }
-//     if (!selectedLang) {
-//         alert('Будь ласка, виберіть мову');
-//         return;
-//     }
-//
-//     var progressBar = document.querySelector('progress');
-//     progressBar.setAttribute('max', '1');
-//     progressBar.setAttribute('value', '0');
-//     progressAnimation.appendChild(progressBar);
-//
-//     Tesseract.recognize(selectedFile, { lang: selectedLang })
-//         .progress(function (message) {
-//             progressBar.setAttribute('value', message.progress);
-//             console.log(message);
-//         })
-//         .catch(function (error) {
-//             console.error(error);
-//         })
-//         .then(function (result) {
-//             resultDiv.textContent = result.text;
-//             resultDiv.removeAttribute("readonly");
-//         });
-// });
-//
-// function copyTextFunction() {
-//     var copyText = document.getElementById("result");
-//     copyText.select();
-//     document.execCommand("copy");
-//     alert("Скопійований текст: " + copyText.value);
-// }
-//
-// // Додайте цей код, щоб очищати локальне сховище при перезавантаженні сторінки перекладу
-// window.addEventListener('load', () => {
-//     const inputTextElem = document.querySelector("#input-text");
-//     const ocrText = localStorage.getItem('ocrText');
-//     if (ocrText) {
-//         setTimeout(() => {
-//             inputTextElem.value = ocrText;
-//             localStorage.removeItem('ocrText'); // Очистити локальне сховище після вставлення тексту
-//             translate(); // Запустити переклад після вставлення тексту
-//         }, 500);
-//     }
-// });
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const selectContainer = document.querySelector('.select-container');
     const selectToggle = selectContainer.querySelector('.select-toggle');
     const selectMenu = selectContainer.querySelector('.select-menu');
     const selectedOption = selectContainer.querySelector('#selected-option');
     const selectElement = selectContainer.querySelector('#lang-select');
-
     let selectedLang;
-
-    // Show/Hide menu
     selectToggle.addEventListener('click', function () {
         selectContainer.classList.toggle('active');
     });
 
-    // Select option
     selectMenu.addEventListener('click', function (e) {
         if (e.target.tagName === 'LI') {
             const value = e.target.getAttribute('data-value');
             const text = e.target.textContent;
             selectedOption.textContent = text;
-            selectElement.value = value; // Set value of hidden select element
-            selectedLang = value; // Update selectedLang with the chosen value
+            selectElement.value = value;
+            selectedLang = value;
             selectMenu.querySelectorAll('li').forEach(li => li.classList.remove('active'));
             e.target.classList.add('active');
             selectContainer.classList.remove('active');
         }
     });
 
-    // Update selected option from the native select
     selectElement.addEventListener('change', function () {
         const value = this.value;
         const text = selectElement.options[selectElement.selectedIndex].text;
@@ -132,7 +35,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Ваш існуючий код
     var fileInput = document.getElementById('file-input');
     var recognizeBtn = document.getElementById('recognize-btn');
     var imagePreview = document.getElementById('image-preview');
@@ -140,14 +42,14 @@ document.addEventListener('DOMContentLoaded', function () {
     var progressAnimation = document.getElementById('progress-container');
     var selectedFile;
 
-    fileInput.addEventListener('change', function(e) {
+    fileInput.addEventListener('change', function (e) {
         selectedFile = e.target.files[0];
         displayImage(selectedFile);
     });
 
     function displayImage(file) {
         var reader = new FileReader();
-        reader.onload = function(event) {
+        reader.onload = function (event) {
             var img = document.createElement('img');
             img.src = event.target.result;
             imagePreview.innerHTML = '';
@@ -156,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
         reader.readAsDataURL(file);
     }
 
-    window.addEventListener('paste', function(e) {
+    window.addEventListener('paste', function (e) {
         var items = e.clipboardData.items;
         for (var i = 0; i < items.length; i++) {
             if (items[i].type.indexOf('image') !== -1) {
@@ -183,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function () {
         progressBar.setAttribute('value', '0');
         progressAnimation.appendChild(progressBar);
 
-        Tesseract.recognize(selectedFile, { lang: selectedLang })
+        Tesseract.recognize(selectedFile, {lang: selectedLang})
             .progress(function (message) {
                 progressBar.setAttribute('value', message.progress);
                 console.log(message);
--- translateScript.js
+++ translateScript.js
@@ -3,92 +3,92 @@ const dropdowns = document.querySelectorAll(".dropdown-container"),
     outputLanguageDropdown = document.querySelector("#output-language");
 
 function populateDropdown(dropdown, options) {
-  dropdown.querySelector("ul").innerHTML = "";
-  options.forEach((option) => {
-    const li = document.createElement("li");
-    const title = option.name + " (" + option.native + ")";
-    li.innerHTML = title;
-    li.dataset.value = option.code;
-    li.classList.add("option");
-    dropdown.querySelector("ul").appendChild(li);
-  });
+    dropdown.querySelector("ul").innerHTML = "";
+    options.forEach((option) => {
+        const li = document.createElement("li");
+        const title = option.name + " (" + option.native + ")";
+        li.innerHTML = title;
+        li.dataset.value = option.code;
+        li.classList.add("option");
+        dropdown.querySelector("ul").appendChild(li);
+    });
 }
 
 populateDropdown(inputLanguageDropdown, languages);
 populateDropdown(outputLanguageDropdown, languages);
 
 dropdowns.forEach((dropdown) => {
-  dropdown.addEventListener("click", (e) => {
-    dropdown.classList.toggle("active");
-  });
-
-  dropdown.querySelectorAll(".option").forEach((item) => {
-    item.addEventListener("click", (e) => {
-      //remove active class from current dropdowns
-      dropdown.querySelectorAll(".option").forEach((item) => {
-        item.classList.remove("active");
-      });
-      item.classList.add("active");
-      const selected = dropdown.querySelector(".selected");
-      selected.innerHTML = item.innerHTML;
-      selected.dataset.value = item.dataset.value;
-      translate();
+    dropdown.addEventListener("click", (e) => {
+        dropdown.classList.toggle("active");
     });
-  });
+
+    dropdown.querySelectorAll(".option").forEach((item) => {
+        item.addEventListener("click", (e) => {
+            //remove active class from current dropdowns
+            dropdown.querySelectorAll(".option").forEach((item) => {
+                item.classList.remove("active");
+            });
+            item.classList.add("active");
+            const selected = dropdown.querySelector(".selected");
+            selected.innerHTML = item.innerHTML;
+            selected.dataset.value = item.dataset.value;
+            translate();
+        });
+    });
 });
 
 document.addEventListener("click", (e) => {
-  dropdowns.forEach((dropdown) => {
-    if (!dropdown.contains(e.target)) {
-      dropdown.classList.remove("active");
-    }
-  });
+    dropdowns.forEach((dropdown) => {
+        if (!dropdown.contains(e.target)) {
+            dropdown.classList.remove("active");
+        }
+    });
 });
 
 const inputTextElem = document.querySelector("#input-text"),
     outputTextElem = document.querySelector("#output-text");
 
 function translate() {
-  const inputText = inputTextElem.value;
-  const inputLanguage =
-      inputLanguageDropdown.querySelector(".selected").dataset.value;
-  const outputLanguage =
-      outputLanguageDropdown.querySelector(".selected").dataset.value;
-  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(
-      inputText
-  )}`;
-  fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        outputTextElem.value = json[0].map((item) => item[0]).join("");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    const inputText = inputTextElem.value;
+    const inputLanguage =
+        inputLanguageDropdown.querySelector(".selected").dataset.value;
+    const outputLanguage =
+        outputLanguageDropdown.querySelector(".selected").dataset.value;
+    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=${outputLanguage}&dt=t&q=${encodeURI(
+        inputText
+    )}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            outputTextElem.value = json[0].map((item) => item[0]).join("");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 inputTextElem.addEventListener("input", (e) => {
-  if (inputTextElem.value.length > 5000) {
-    inputTextElem.value = inputTextElem.value.slice(0, 5000);
-  }
-  translate();
+    if (inputTextElem.value.length > 5000) {
+        inputTextElem.value = inputTextElem.value.slice(0, 5000);
+    }
+    translate();
 });
 
 const downloadBtn = document.querySelector("#download-btn");
 
 downloadBtn.addEventListener("click", (e) => {
-  const outputText = outputTextElem.value;
-  const outputLanguage =
-      outputLanguageDropdown.querySelector(".selected").dataset.value;
-  if (outputText) {
-    const blob = new Blob([outputText], { type: "text/plain" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.download = `translated-to-${outputLanguage}.txt`;
-    a.href = url;
-    a.click();
-  }
+    const outputText = outputTextElem.value;
+    const outputLanguage =
+        outputLanguageDropdown.querySelector(".selected").dataset.value;
+    if (outputText) {
+        const blob = new Blob([outputText], {type: "text/plain"});
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.download = `translated-to-${outputLanguage}.txt`;
+        a.href = url;
+        a.click();
+    }
 });
 
 
